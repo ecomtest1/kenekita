5f856490c649d412bc44aa437d5cfd096fe39679
BonacinaAndrea <bonacinaandrea90@gmail.com>
2026-05-12 09:42:07 +0200

    Aggiunto token Tina per admin

--- tina/config.ts
+++ tina/config.ts
@@ -5,7 +5,7 @@ import { pageCollection } from "./collections/page";
 export default defineConfig({
   branch: "main",
   clientId: "0c732506-cf9e-4409-b879-dff0af8e922d",
-  token: null,
+  token: "4dbf0dd491bd8953c2c5719abcd981a8362f78b2",
   build: {
     outputFolder: "admin",
     publicFolder: "public",
